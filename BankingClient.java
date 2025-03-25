402c6ab1cfec43e84d25554e6a07170d37161146
asanketghugare <sanketghugare0103@gmail.com>
2025-03-26 02:26:04 +0530

    used type casting for use derived class method

--- BankingClient.java
+++ BankingClient.java
@@ -1,12 +1,18 @@
 import com.devgen.banking.model.Account;
+import com.devgen.banking.model.CurrentAccount;
 import com.devgen.banking.model.SavingAccount;
 
 public class BankingClient {
     public static void main(String[] args) {
 
-    Account account = new SavingAccount(123456,500);
-        System.out.println(account.getAccountNo() +" "+ account.getBalance()+" "+account.getAccountType());
+    Account savingAccountaccount = new SavingAccount(123456,500);
+        System.out.println("Account : "+ savingAccountaccount.getAccountNo() +" Account No : "+ savingAccountaccount.getBalance()+" Account Type : "+ savingAccountaccount.getAccountType());
 
+     Account currentAccount = new CurrentAccount(51422,2000);
 
+     CurrentAccount currentAccountRef = (CurrentAccount) currentAccount;
+     
+        System.out.println("Account : "+ savingAccountaccount.getAccountNo() +" Account No : "+ savingAccountaccount.getBalance()+" Account Type : "+ savingAccountaccount.getAccountType()+
+                " Overdraft Limit : "+currentAccountRef.getOverdraftLimit());
     }
 }
--- com/devgen/banking/model/CurrentAccount.java
+++ com/devgen/banking/model/CurrentAccount.java
@@ -2,7 +2,15 @@ package com.devgen.banking.model;
 
 public class CurrentAccount extends Account {
 
+    private final double overdraftLimit = 500;
+
     public CurrentAccount(long accountNo, double balance){
         super(accountNo, balance, AccountType.CURRENT);
     }
+
+    public double getOverdraftLimit(){
+        return overdraftLimit;
+
+    }
 }
+
